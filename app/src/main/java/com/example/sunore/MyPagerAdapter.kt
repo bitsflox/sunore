5a24672bf9b3da356fec88044984f0f6b7d335ce
anil494 <mryoungrawat@gmail.com>
2020-10-22 17:06:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunore/MyPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.sunore
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                PendingOrdersFragment()
+            }
+            1 -> ConfirmedOrdersFragment()
+            else -> {
+                return DeliveredOrdersFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "First Tab"
+            1 -> "Second Tab"
+            else -> {
+                return "Third Tab"
+            }
+        }
+    }
+}
